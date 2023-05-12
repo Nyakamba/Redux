4feb09c64f5eedfc5021fc38968c1f85bff10b62
Nyakamba <omwegaenock@gmail.com>
2023-05-12 11:24:19 +0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = " ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = " ICECREAM_RESTOCKED";
 
 function orderCake() {
   return {
@@ -10,18 +14,71 @@ function orderCake() {
   };
 }
 
-const initalState = {
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+
+function orderIceCream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockIceCream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
+// const initalState = {
+//   numOfcakes: 10,
+//   numOfIceCreams: 20,
+// };
+
+const initialCakeState = {
   numOfcakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 20,
 };
 
 //(previousState, action) =>newState
 
-const reducer = (state = initalState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
         ...state, //use the spread operator when the state has more than one object as its initial property
         numOfcakes: state.numOfcakes - 1,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfcakes: state.numOfcakes + action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state, //use the spread operator when the state has more than one object as its initial property
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
       };
     default:
       return state;
@@ -34,10 +91,21 @@ console.log("Initial state", store.getState());
 const unsubscribe = store.subscribe(() =>
   console.log("updated state", store.getState())
 );
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockCake(3));
+
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIceCream, restockIceCream },
+  store.dispatch
+);
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(3);
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(2);
 
 unsubscribe();
-store.dispatch(orderCake());
-store.dispatch(orderCake());
