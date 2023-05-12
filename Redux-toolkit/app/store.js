98a2fbe049e2c69d35874ec233564d75ad9719cb
Nyakamba <omwegaenock@gmail.com>
2023-05-12 21:58:07 +0300

    store.js

--- Redux-toolkit/app/store.js
+++ Redux-toolkit/app/store.js
@@ -2,6 +2,7 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 //const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
+const useReducer = require("../features/user/userSlice");
 
 //const logger = reduxLogger.createLogger();
 
@@ -9,6 +10,7 @@ const store = configureStore({
   reducer: {
     cake: cakeReducer,
     icecream: icecreamReducer,
+    user: useReducer,
   },
   // middleware: (getdefaultMiddleware) => getdefaultMiddleware().concat(logger),
 });
